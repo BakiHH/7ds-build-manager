9c55b0f3e4e442e836274899ac0d206678bddb94
Baki <andre.berger99@web.de>
2026-06-11 20:56:59 +0200

    up

--- scipt.js
+++ scipt.js
@@ -1,115 +1,53 @@
-let characters = [];
+let data = [];
 
 async function init() {
-    const res = await fetch("characters.json");
-    characters = await res.json();
+    const res = await fetch("database.json");
+    data = await res.json();
 
-    const select = document.getElementById("characterSelect");
+    renderList();
+    loadChar(0);
 
-    characters.forEach(c => {
-        let opt = document.createElement("option");
-        opt.value = c.id;
-        opt.textContent = c.name;
-        select.appendChild(opt);
+    search.addEventListener("input", filter);
+}
+
+function renderList() {
+    const list = document.getElementById("charList");
+    list.innerHTML = "";
+
+    data.forEach((c, i) => {
+        let div = document.createElement("div");
+        div.className = "char-btn";
+        div.innerText = c.name;
+        div.onclick = () => loadChar(i);
+        list.appendChild(div);
     });
-
-    select.addEventListener("change", loadBuild);
-    document.getElementById("search").addEventListener("input", filterChars);
-
-    loadBuild();
 }
 
-function getChar() {
-    return document.getElementById("characterSelect").value;
+function loadChar(i) {
+    const c = data[i];
+
+    charImg.src = c.image;
+    charName.innerText = c.name;
+    charRole.innerText = c.role;
+
+    engraved.innerText = c.engraved;
+    chest.innerText = c.chest;
+    legs.innerText = c.legs;
+    boots.innerText = c.boots;
+    belt.innerText = c.belt;
+    earrings.innerText = c.earrings;
+    necklace.innerText = c.necklace;
+    ring.innerText = c.ring;
+
+    notes.innerText = c.notes;
 }
 
-function saveBuild() {
-    let data = collect();
+function filter() {
+    const val = search.value.toLowerCase();
 
-    localStorage.setItem(getChar(), JSON.stringify(data));
-    updateScore();
-}
-
-function collect() {
-    return {
-        role: role.value,
-        mainWeapon: mainWeapon.value,
-        subWeapon: subWeapon.value,
-        chest: chest.value,
-        legs: legs.value,
-        boots: boots.value,
-        belt: belt.value,
-        earrings: earrings.value,
-        necklace: necklace.value,
-        ring: ring.value,
-        notes: notes.value
-    };
-}
-
-function loadBuild() {
-    let data = localStorage.getItem(getChar());
-
-    let char = characters.find(c => c.id === getChar());
-    document.getElementById("characterImage").src = char.image;
-
-    if (!data) return;
-
-    let b = JSON.parse(data);
-
-    Object.keys(b).forEach(k => {
-        if (document.getElementById(k)) {
-            document.getElementById(k).value = b[k];
-        }
-    });
-
-    updateScore();
-}
-
-function updateScore() {
-    let b = collect();
-
-    let score =
-        (b.mainWeapon?.length || 0) +
-        (b.subWeapon?.length || 0) +
-        (b.chest?.length || 0) +
-        (b.legs?.length || 0) +
-        (b.boots?.length || 0) +
-        (b.belt?.length || 0) +
-        (b.earrings?.length || 0) +
-        (b.necklace?.length || 0) +
-        (b.ring?.length || 0);
-
-    document.getElementById("score").innerText = score;
-}
-
-function exportBuild() {
-    let data = JSON.stringify(collect());
-    let blob = new Blob([data], {type:"application/json"});
-    let a = document.createElement("a");
-
-    a.href = URL.createObjectURL(blob);
-    a.download = getChar() + "_build.json";
-    a.click();
-}
-
-document.getElementById("importFile").addEventListener("change", e => {
-    let file = e.target.files[0];
-
-    let reader = new FileReader();
-    reader.onload = function(ev) {
-        localStorage.setItem(getChar(), ev.target.result);
-        loadBuild();
-    };
-
-    reader.readAsText(file);
-});
-
-function filterChars() {
-    let val = search.value.toLowerCase();
-    let select = characterSelect;
-
-    [...select.options].forEach(o => {
-        o.style.display = o.text.toLowerCase().includes(val) ? "" : "none";
+    document.querySelectorAll(".char-btn").forEach(btn => {
+        btn.style.display =
+            btn.innerText.toLowerCase().includes(val) ? "" : "none";
     });
 }
 
